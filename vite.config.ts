688251e770a142b83d7988586d174cdaebd0cffd
Alex Chiu <alexchiu11@gmail.com>
2025-09-28 23:11:57 +0100

    Refactor package dependencies and remove unused Vite configuration

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [],
-  optimizeDeps: {
-    include: ["shiki-magic-move/core", "shiki-magic-move/renderer"],
-  },
-});
